84dbfc00f6ae8157104fe2e1dc8eb96d8de5653e
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-04-10 21:38:39 -0600

    Fixed: only one drag menu can now appear at a time

--- src/app/components/IconButton/index.tsx
+++ src/app/components/IconButton/index.tsx
@@ -1,18 +1,19 @@
 import React, { forwardRef } from "react";
 
 interface Props {
+	id: string;
 	selected?: boolean;
 	icon: React.ReactNode;
 	onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const IconButton = forwardRef<HTMLInputElement, Props>(
-	({ selected = false, icon, onClick }, ref) => {
+	({ id, selected, icon, onClick }, ref) => {
 		let className = "NLT__button--reset";
 		if (selected) className += " NLT__selected";
 
 		return (
-			<button className={className} ref={ref} onClick={onClick}>
+			<button id={id} className={className} ref={ref} onClick={onClick}>
 				{icon}
 			</button>
 		);
