a5dead0f4f6bc45f085c7f88fed4048223c9f3ce
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-05-15 19:24:27 -0600

    Add menu provider

--- src/app/components/MenuProvider/index.tsx
+++ src/app/components/MenuProvider/index.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 const MenuContext = React.createContext({});
+
+export const useMenu = () => {
+	return useContext(MenuContext);
+};
 
 interface Props {
 	children: React.ReactNode;
 }
 
 export default function MenuProvider({ children }: Props) {
-	return <MenuContext.Provider value={{}}>{children}</MenuContext.Provider>;
+	const [openMenus, setOpenMenus] = useState([]);
+
+	function openMenu(id: string, level: number) {
+		const menu = { id, level };
+		setOpenMenus((prevState) => [...prevState, menu]);
+	}
+
+	function closeMenu(id: string) {
+		setOpenMenus((prevState) => prevState.filter((menu) => menu.id !== id));
+	}
+
+	return (
+		<MenuContext.Provider value={{ openMenus, openMenu, closeMenu }}>
+			{children}
+		</MenuContext.Provider>
+	);
 }
