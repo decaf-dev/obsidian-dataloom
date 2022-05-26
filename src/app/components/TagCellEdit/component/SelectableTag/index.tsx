3cd22cc92b2043b343fd350538353d678c335269
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-05-26 10:48:23 -0600

    Fix tag menu display

--- src/app/components/TagCellEdit/component/SelectableTag/index.tsx
+++ src/app/components/TagCellEdit/component/SelectableTag/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import parse from "html-react-parser";
 
@@ -11,6 +11,7 @@ import "./styles.css";
 import IconButton from "src/app/components/IconButton";
 import TagColorMenu from "src/app/components/TagColorMenu";
 import { useMenu } from "src/app/components/MenuProvider";
+import { useResizeTime } from "src/app/services/hooks";
 interface Props {
 	id: string;
 	content: string;
@@ -26,10 +27,25 @@ export default function SelectableTag({
 	onClick,
 	onColorChange,
 }: Props) {
+	const [menuPosition, setMenuPosition] = useState({
+		top: 0,
+		left: 0,
+	});
 	const [menuId] = useState(uuidv4());
 	const { isOpen, open, close } = useMenu(menuId, MENU_LEVEL.TWO);
+	const resizeTime = useResizeTime();
 	let tagClass = "NLT__tag";
 	tagClass += " " + findColorClass(color);
+
+	const divRef = useCallback(
+		(node) => {
+			if (node instanceof HTMLElement) {
+				const { top, left } = node.getBoundingClientRect();
+				setMenuPosition({ top, left });
+			}
+		},
+		[isOpen, resizeTime]
+	);
 
 	function handleColorChange(color: string) {
 		onColorChange(id, color);
@@ -37,6 +53,7 @@ export default function SelectableTag({
 	}
 	return (
 		<div
+			ref={divRef}
 			className="NLT__selectable-tag NLT__selectable"
 			onClick={() => onClick(id)}
 		>
@@ -54,8 +71,8 @@ export default function SelectableTag({
 				menuId={menuId}
 				isOpen={isOpen}
 				selectedColor={color}
-				top={-100}
-				left={-120}
+				top={menuPosition.top - 77}
+				left={menuPosition.left + 110}
 				onColorClick={(color) => handleColorChange(color)}
 			/>
 		</div>
