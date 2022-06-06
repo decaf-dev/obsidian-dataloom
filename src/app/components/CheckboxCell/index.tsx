757c5ded9d65844b357a9b71459082b7cb59ad32
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-06-05 18:02:15 -0600

    Fix checkbox click

--- src/app/components/CheckboxCell/index.tsx
+++ src/app/components/CheckboxCell/index.tsx
@@ -17,7 +17,6 @@ export default function CheckboxCell({ isChecked, onCheckboxChange }: Props) {
 				className="task-list-item-checkbox"
 				type="checkbox"
 				checked={isChecked}
-				onChange={() => onCheckboxChange(!isChecked)}
 			/>
 		</div>
 	);
