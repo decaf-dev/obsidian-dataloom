4d7ebc4bee356fb3e6825ff64d2b8a0ce53b9794
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-09-26 20:44:13 -0600

    Added: close button on tag color menu

--- src/components/TagColorMenu/index.tsx
+++ src/components/TagColorMenu/index.tsx
@@ -4,7 +4,10 @@ import Menu from "src/components/Menu";
 
 import { COLOR } from "src/constants";
 import ColorItem from "./components/ColorItem";
+import { Close } from "@mui/icons-material";
 import "./styles.css";
+import { useAppDispatch } from "src/services/redux/hooks";
+import { closeTopLevelMenu } from "src/services/menu/menuSlice";
 
 interface Props {
 	menuId: string;
@@ -24,10 +27,23 @@ export default function TagColorMenu({
 	selectedColor,
 	onColorClick,
 }: Props) {
+	const dispatch = useAppDispatch();
+	function handleMenuCloseClick(e: React.MouseEvent) {
+		//TODO this is more of a hack than a fix
+		e.stopPropagation();
+		dispatch(closeTopLevelMenu());
+	}
+
 	return (
 		<Menu id={menuId} isOpen={isOpen} style={style}>
 			<div className="NLT__tag-color-menu">
-				<div className="NLT__tag-color-menu-title">Colors</div>
+				<div className="NLT__tag-color-menu-header">
+					<div className="NLT__tag-color-menu-title">Colors</div>
+					<Close
+						className="NLT__icon--md NLT__icon--selectable"
+						onClick={handleMenuCloseClick}
+					/>
+				</div>
 				<div className="NLT__tag-color-container">
 					{Object.values(COLOR).map((color) => (
 						<ColorItem
