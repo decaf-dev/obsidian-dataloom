d8a7167a010446dcbb6105223ed36c54a3e92362
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2022-10-01 16:18:15 -0600

    Refactored: removed unused code

--- src/services/string/validators.ts
+++ src/services/string/validators.ts
@@ -11,10 +11,6 @@ const isMatch = (input: string, regex: RegExp): boolean => {
 	return (input.match(regex) || []).length !== 0;
 };
 
-const countNumMatch = (input: string, regex: RegExp): number => {
-	return (input.match(regex) || []).length;
-};
-
 export const isNumber = (input: string): boolean => {
 	return isMatch(input, NUMBER_REGEX);
 };
@@ -33,13 +29,4 @@ export const isTag = (input: string): boolean => {
 
 export const isCheckboxChecked = (input: string): boolean => {
 	return isMatch(input, CHECKBOX_CHECKED_REGEX);
-};
-
-/**
- * Counts the number of tags in a string.
- * @param input The input string
- * @returns The number of tags in the input string
- */
-export const countNumTags = (input: string): number => {
-	return countNumMatch(input, TAGS_REGEX);
 };
