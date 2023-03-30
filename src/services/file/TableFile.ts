b2f7cf1371298aa50e2befc600c8339db5060594
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-03-30 01:36:16 -0600

    fix: load actual file data

--- src/services/file/TableFile.ts
+++ src/services/file/TableFile.ts
@@ -1,14 +1,19 @@
 import { Notice } from "obsidian";
+import { mockTableState } from "../mock";
 import FileOperations from "./File";
+import Json from "./Json";
 
 export const TABLE_EXTENSION = "table";
 export default class TableFile {
 	static async createNotionLikeTableFile() {
 		try {
 			const filePath = "Untitled" + "." + TABLE_EXTENSION;
+
+			const tableState = mockTableState(1, 1);
+			const serialized = Json.serializeTableState(tableState);
 			const updatedFilePath = await FileOperations.createFile(
 				filePath,
-				"{}"
+				serialized
 			);
 			return updatedFilePath;
 		} catch (err) {
