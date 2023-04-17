e17d9f80a1d41d6eb574efc9feb8a729c9d41fc3
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-04-17 13:15:16 -0600

    fix: render all new lines

--- src/services/markdown/utils.ts
+++ src/services/markdown/utils.ts
@@ -1,9 +1,11 @@
 export const replaceNewLinesWithBreakTag = (markdown: string) => {
 	let endsWithNewLine = false;
 	if (markdown.endsWith("\n")) endsWithNewLine = true;
+
 	const updated = markdown.replace(/\n/g, "<br>");
+
 	//If we end with a \n tag then add an extra break tag.
-	//This is because the MarkdownRenderer.renderMarkdown function doesn't rendr the last \n
+	//This is because the MarkdownRenderer.renderMarkdown function won't render the last <br> tag
 	if (endsWithNewLine) return updated + "<br>";
 	return updated;
 };
