988a31310359491d11b3f4b339cdfd1031632662
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-04-21 20:00:14 -0600

    fix: make switch authentic to Obsidian

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,10 +1,12 @@
+import "./styles.css";
+
 interface Props {
 	isChecked: boolean;
 	onToggle: (value: boolean) => void;
 }
 
 export default function Switch({ isChecked, onToggle }: Props) {
-	let className = "checkbox-container";
+	let className = "checkbox-container NLT__switch";
 	if (isChecked) className += " is-enabled";
 	return (
 		<div className={className} onClick={() => onToggle(!isChecked)}>
