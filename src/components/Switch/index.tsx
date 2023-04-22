229a5d70b05013e57b9a9cc43b54310cdccfd9c5
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-04-21 19:26:30 -0600

    fix: stop flash on open with menu with a switch component

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,22 +1,14 @@
-import "./styles.css";
-
 interface Props {
 	isChecked: boolean;
 	onToggle: (value: boolean) => void;
 }
 
 export default function Switch({ isChecked, onToggle }: Props) {
+	let className = "checkbox-container";
+	if (isChecked) className += " is-enabled";
 	return (
-		<label className="NLT__switch">
-			<input
-				type="checkbox"
-				checked={isChecked}
-				onChange={() => {}}
-				onClick={() => {
-					onToggle(!isChecked);
-				}}
-			/>
-			<span className="NLT__slider"></span>
-		</label>
+		<div className={className} onClick={() => onToggle(!isChecked)}>
+			<input type="checkbox" />
+		</div>
 	);
 }
