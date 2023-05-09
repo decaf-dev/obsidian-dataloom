1b67c1abc7fb8924434a53c49353aedf071e53ca
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-09 02:16:26 -0600

    fix: resolve text to number

--- src/react/table-app/number-cell/index.tsx
+++ src/react/table-app/number-cell/index.tsx
@@ -1,6 +1,7 @@
 import { useOverflowClassName } from "src/shared/spacing/hooks";
 
 import "./styles.css";
+import { isNumber } from "src/shared/validators";
 
 interface Props {
 	value: string;
@@ -11,5 +12,10 @@ export default function NumberCell({ value, shouldWrapOverflow }: Props) {
 	const overflowClassName = useOverflowClassName(shouldWrapOverflow);
 	const className = "NLT__number-cell" + " " + overflowClassName;
 
-	return <div className={className}>{value}</div>;
+	let valueFormatted = "";
+	if (isNumber(value)) {
+		valueFormatted = value;
+	}
+
+	return <div className={className}>{valueFormatted}</div>;
 }
