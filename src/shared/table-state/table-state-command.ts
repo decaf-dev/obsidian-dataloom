79631637ca04232dec2bd140eabbcc56b74a06aa
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-14 18:35:18 -0600

    refactor: [WIP] update TableStateCommand structure

--- src/shared/table-state/table-state-command.ts
+++ src/shared/table-state/table-state-command.ts
@@ -1,7 +1,25 @@
+import { CommandRedoError, CommandUndoError } from "../commands/command-errors";
 import { TableState } from "./types";
 
-export default interface TableStateCommand {
-	execute(prevState: TableState): TableState;
-	redo?(prevState: TableState): TableState;
-	undo(prevState: TableState): TableState;
+export default abstract class TableStateCommand {
+	hasExecuteBeenCalled: boolean = false;
+	hasUndoBeenCalled: boolean = false;
+
+	onExecute() {
+		this.hasExecuteBeenCalled = true;
+	}
+
+	onRedo() {
+		if (!this.hasUndoBeenCalled) throw new CommandRedoError();
+		this.hasUndoBeenCalled = false;
+	}
+
+	onUndo() {
+		if (!this.hasExecuteBeenCalled) throw new CommandUndoError();
+		this.hasUndoBeenCalled = true;
+	}
+
+	abstract execute(prevState: TableState): TableState;
+	abstract redo(prevState: TableState): TableState;
+	abstract undo(prevState: TableState): TableState;
 }
