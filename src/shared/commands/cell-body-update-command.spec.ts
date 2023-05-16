66163ab199e53063e748eac6a926527fe86c616f
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-15 23:46:38 -0600

    refactor: replace timer with jest-date-mock library

--- src/shared/commands/cell-body-update-command.spec.ts
+++ src/shared/commands/cell-body-update-command.spec.ts
@@ -1,44 +1,26 @@
 import { createTableState } from "src/data/table-state-factory";
 import { CommandUndoError } from "./command-errors";
 import CellBodyUpdateCommand from "./cell-body-update-command";
+import { advanceBy, clear } from "jest-date-mock";
 
 describe("row-body-update-command", () => {
 	it("should throw an error when undo() is called before execute()", () => {
 		try {
 			const prevState = createTableState(1, 1);
-			new CellBodyUpdateCommand(
+			const command = new CellBodyUpdateCommand(
 				prevState.model.bodyCells[0].id,
 				prevState.model.bodyRows[0].id,
 				"markdown",
 				"test"
-			).undo(prevState);
+			);
+			command.undo(prevState);
 		} catch (err) {
 			expect(err).toBeInstanceOf(CommandUndoError);
 		}
 	});
 
 	it("should update a cell property when execute() is called", async () => {
-		const prevState = createTableState(1, 1);
-
-		//Wait so that the time will be different
-		await new Promise((resolve) => setTimeout(resolve, 5));
-
-		const executeState = new CellBodyUpdateCommand(
-			prevState.model.bodyCells[0].id,
-			prevState.model.bodyRows[0].id,
-			"markdown",
-			"test"
-		).execute(prevState);
-
-		expect(executeState.model.bodyRows.length).toEqual(1);
-		expect(executeState.model.bodyCells.length).toEqual(1);
-		expect(executeState.model.bodyCells[0].markdown).toEqual("test");
-		expect(executeState.model.bodyRows[0].lastEditedTime).not.toEqual(
-			prevState.model.bodyRows[0].lastEditedTime
-		);
-	});
-
-	it("should reset the cell property when undo() is called", () => {
+		//Arrange
 		const prevState = createTableState(1, 1);
 		const command = new CellBodyUpdateCommand(
 			prevState.model.bodyCells[0].id,
@@ -46,9 +28,36 @@ describe("row-body-update-command", () => {
 			"markdown",
 			"test"
 		);
+
+		//Act
+		advanceBy(100);
+		const executeState = command.execute(prevState);
+		clear();
+
+		//Assert
+		expect(executeState.model.bodyRows.length).toEqual(1);
+		expect(executeState.model.bodyCells.length).toEqual(1);
+		expect(executeState.model.bodyCells[0].markdown).toEqual("test");
+		expect(executeState.model.bodyRows[0].lastEditedTime).toBeGreaterThan(
+			prevState.model.bodyRows[0].lastEditedTime
+		);
+	});
+
+	it("should reset the cell property when undo() is called", () => {
+		//Arrange
+		const prevState = createTableState(1, 1);
+		const command = new CellBodyUpdateCommand(
+			prevState.model.bodyCells[0].id,
+			prevState.model.bodyRows[0].id,
+			"markdown",
+			"test"
+		);
+
+		//Act
 		const executeState = command.execute(prevState);
 		const undoState = command.undo(executeState);
 
+		//Assert
 		expect(undoState.model.bodyRows).toEqual(prevState.model.bodyRows);
 		expect(undoState.model.bodyCells).toEqual(prevState.model.bodyCells);
 	});
