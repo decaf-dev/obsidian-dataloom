b4a82467a17565a238299c1b95379353e99636a1
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-18 17:21:12 -0600

    fix: add touch events for mobile

---
+++ src/react/table-app/header-cell/resize-container.tsx
@@ -0,0 +1,67 @@
+import { numToPx, pxToNum } from "src/shared/conversion";
+import { useColumnResize } from "./use-column-resize";
+import { MIN_COLUMN_WIDTH } from "src/shared/table-state/constants";
+import { css } from "@emotion/react";
+
+const containerStyle = css`
+	position: relative;
+`;
+
+const innerStyle = css`
+	position: absolute;
+	left: -5px;
+	cursor: col-resize;
+	width: 8px;
+	height: 100%;
+	&:hover {
+		background-color: var(--interactive-accent);
+	}
+`;
+
+const activeStyle = css`
+	background-color: var(--interactive-accent);
+`;
+
+interface Props {
+	currentResizingId: string | null;
+	columnId: string;
+	width: string;
+	onWidthChange: (columnId: string, value: string) => void;
+}
+
+export default function ResizeContainer({
+	currentResizingId,
+	columnId,
+	width,
+	onWidthChange,
+}: Props) {
+	const { handleMouseDown, handleTouchStart } = useColumnResize(
+		columnId,
+		(dist) => {
+			const oldWidth = pxToNum(width);
+			const newWidth = oldWidth + dist;
+
+			if (newWidth < MIN_COLUMN_WIDTH) return;
+			onWidthChange(columnId, numToPx(newWidth));
+		}
+	);
+
+	const isActive = columnId == currentResizingId;
+
+	return (
+		<div css={containerStyle}>
+			<div
+				css={[innerStyle, isActive && activeStyle]}
+				onMouseDown={handleMouseDown}
+				onTouchStart={handleTouchStart}
+				onClick={(e) => {
+					//Stop propagation so we don't open the header
+					e.stopPropagation();
+
+					//If the user is double clicking then set width to max
+					if (e.detail === 2) onWidthChange(columnId, "unset");
+				}}
+			/>
+		</div>
+	);
+}
---
+++ src/react/table-app/header-cell/use-column-resize.ts
@@ -0,0 +1,77 @@
+import { useRef } from "react";
+import { setResizingColumnId } from "src/redux/global/global-slice";
+import { useAppDispatch } from "src/redux/global/hooks";
+
+export const useColumnResize = (
+	columnId: string,
+	onMove: (dist: number) => void
+) => {
+	//The x position of the mouse when it is pressed down
+	//This should be the same for both mouse and touch events
+	const mouseDownX = useRef(0);
+
+	const dispatch = useAppDispatch();
+
+	function handleMouseMove(e: MouseEvent) {
+		const dist = e.pageX - mouseDownX.current;
+		onMove(dist);
+	}
+
+	function handleTouchMove(e: TouchEvent) {
+		const dist = e.touches[0].pageX - mouseDownX.current;
+		onMove(dist);
+	}
+
+	function handleMouseUp() {
+		document.removeEventListener("mousemove", handleMouseMove);
+		document.removeEventListener("mouseup", handleMouseUp);
+
+		//Prevents the column menu from opening when the user releases the mouse
+		setTimeout(() => {
+			dispatch(setResizingColumnId(null));
+		}, 100);
+	}
+
+	function handleTouchEnd() {
+		document.removeEventListener("touchmove", handleTouchMove);
+		document.removeEventListener("touchend", handleTouchEnd);
+
+		//Prevents the column menu from opening when the user releases the mouse
+		setTimeout(() => {
+			dispatch(setResizingColumnId(null));
+		}, 100);
+	}
+
+	function handleTouchStart(e: React.TouchEvent) {
+		//Mouse event
+		e.preventDefault();
+
+		//Add event listeners
+		document.addEventListener("touchmove", handleTouchMove);
+		document.addEventListener("touchend", handleTouchEnd);
+
+		//Set the current mouse position, this will be used to calculate the distance
+		//the touch has moved
+		mouseDownX.current = e.touches[0].pageX;
+		dispatch(setResizingColumnId(columnId));
+	}
+
+	function handleMouseDown(e: React.MouseEvent) {
+		//If we double click, then don't resize
+		if (e.detail >= 2) return;
+
+		//Prevent drag and drop
+		e.preventDefault();
+
+		//Add event listeners
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+
+		//Set the current mouse position, this will be used to calculate the distance
+		//the mouse has moved
+		mouseDownX.current = e.pageX;
+		dispatch(setResizingColumnId(columnId));
+	}
+
+	return { handleMouseDown, handleTouchStart };
+};
