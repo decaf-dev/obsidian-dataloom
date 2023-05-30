de2779359f42c6f3b698372ff2b3f4a212599236
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-30 13:41:23 -0600

    fix: resolve input breaking when multiple tables are open

--- src/shared/event-system/hooks.ts
+++ src/shared/event-system/hooks.ts
@@ -1,15 +1,23 @@
 import React from "react";
 import { eventSystem } from "./event-system";
+import { NLTView } from "src/obsidian/nlt-view";
+import { useViewContext } from "../view-context";
 
 export const useEventSystem = () => {
+	const view = useViewContext();
 	React.useEffect(() => {
 		function handleKeyDown(e: KeyboardEvent) {
-			eventSystem.dispatchEvent("keydown", e);
+			const activeView = app.workspace.getActiveViewOfType(NLTView);
+			if (!activeView) return;
+
+			if (activeView.leaf === view.leaf) {
+				eventSystem.dispatchEvent("keydown", e);
+			}
 		}
 
 		document.addEventListener("keydown", handleKeyDown);
 		return () => {
 			document.removeEventListener("keydown", handleKeyDown);
 		};
-	}, []);
+	}, [view]);
 };
