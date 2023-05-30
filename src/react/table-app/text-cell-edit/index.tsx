7db323218c19bf991583dc1c4770d90401de079d
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-30 11:36:09 -0600

    fix: add line break on enter press

--- src/react/table-app/text-cell-edit/index.tsx
+++ src/react/table-app/text-cell-edit/index.tsx
@@ -40,7 +40,7 @@ export default function TextCellEdit({
 	const previousValue = useRef("");
 
 	function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-		if (e.key === "Enter") e.preventDefault();
+		if (e.key === "Enter" && !e.shiftKey) e.preventDefault();
 	}
 
 	function handleTextareaChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
