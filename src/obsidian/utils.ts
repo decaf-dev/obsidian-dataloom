d33d348a608eaacc25324279f11316a9da727ea5
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-06-03 12:15:08 -0600

    fix: update table on width or height change

--- src/obsidian/utils.ts
+++ src/obsidian/utils.ts
@@ -1,4 +1,5 @@
 import { TABLE_EXTENSION } from "src/data/constants";
+import { numToPx } from "src/shared/conversion";
 
 export const getEmbeddedTableLinkEls = (el: HTMLElement) => {
 	const embeddedLinkEls = el.querySelectorAll(".internal-embed");
@@ -11,4 +12,37 @@ export const getEmbeddedTableLinkEls = (el: HTMLElement) => {
 			tableLinkEls.push(linkEl as HTMLElement);
 	}
 	return tableLinkEls;
+};
+
+export const removeEmbeddedLinkChildren = (embeddedLinkEl: HTMLElement) => {
+	if (embeddedLinkEl.children.length > 0) {
+		const firstChildEl = embeddedLinkEl.children[0];
+		if (firstChildEl.classList.contains("NLT__embedded-container")) return;
+
+		//Remove any default children that Obsidian has created
+		for (let i = 0; i < embeddedLinkEl.children.length; i++) {
+			embeddedLinkEl.removeChild(embeddedLinkEl.children[i]);
+		}
+	}
+};
+
+export const findEmbeddedTableFile = (embeddedLinkEl: HTMLElement) => {
+	const src = embeddedLinkEl.getAttribute("src");
+
+	const tableFile = app.vault.getFiles().find((file) => file.path === src);
+	if (tableFile === undefined)
+		return app.vault.getFiles().find((file) => file.name === src) ?? null;
+	return tableFile ?? null;
+};
+
+export const getEmbeddedTableWidth = (embeddedLinkEl: HTMLElement) => {
+	const width = embeddedLinkEl.getAttribute("width");
+	if (width === null || width === "1") return "100%";
+	return numToPx(width);
+};
+
+export const getEmbeddedTableHeight = (embeddedLinkEl: HTMLElement) => {
+	const height = embeddedLinkEl.getAttribute("height");
+	if (height === null || height === "1") return "340px"; //exactly 4 normal body rows
+	return numToPx(height);
 };
