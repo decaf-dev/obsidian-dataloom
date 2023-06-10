e317b0e9e2b5534a3afb94a082f5b37f165bfccf
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-06-09 21:18:05 -0600

    feat: migrate old config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,17 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: { node: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": "warn",
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"react-hooks/exhaustive-deps": "error",
+	},
+};
