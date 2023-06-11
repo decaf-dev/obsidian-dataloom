c6c36f11291ff192096e29ddffb6a2592684ee94
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-06-11 16:50:19 -0600

    test: add tests for cell content

---
+++ src/shared/cell-content/time-content.spec.ts
@@ -0,0 +1,14 @@
+import { DateFormat } from "../types";
+import { getTimeCellContent } from "./time-content";
+
+describe("getTimeCellContent", () => {
+	it("should return a datetime string if the time is a number", () => {
+		const result = getTimeCellContent(1704006000000, DateFormat.MM_DD_YYYY);
+		expect(result).toEqual("12/31/2023 12:00 AM");
+	});
+
+	it("should return an empty string if time is null", () => {
+		const result = getTimeCellContent(null, DateFormat.DD_MM_YYYY);
+		expect(result).toEqual("");
+	});
+});
