be96ae6457d16b95974f4df6d94d57fef15166e5
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-07-04 12:49:56 -0600

    refactor: update social card

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			// Replace with your project's social card
-			image: "img/docusaurus-social-card.jpg",
+			image: "img/social-card.png",
 			blog: false,
 			navbar: {
 				title: "Dashboards",
