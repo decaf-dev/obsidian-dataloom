5cbe821650513eabbe29d429ee99c31739923062
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-07-04 13:19:25 -0600

    feat: add built site

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "trey-wallis", // Usually your GitHub org/user name.
-	projectName: "Dashboards", // Usually your repo name.
+	projectName: "obsidian-dashboards", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
