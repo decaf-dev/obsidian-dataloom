fbbe28bb34892b147783ebec2fe3917025477981
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-07-07 15:12:20 -0600

    refactor: make new button a plus

--- src/react/dashboard-app/new-row-button/index.tsx
+++ src/react/dashboard-app/new-row-button/index.tsx
@@ -1,6 +1,6 @@
+import { css } from "@emotion/react";
 import Button from "../../shared/button";
-
-import "./styles.css";
+import Icon from "src/react/shared/icon";
 
 interface Props {
 	onClick: () => void;
@@ -8,8 +8,16 @@ interface Props {
 
 export default function NewRowButton({ onClick }: Props) {
 	return (
-		<div className="Dashboards__new-row">
-			<Button onClick={() => onClick()}>New row</Button>
+		<div
+			css={css`
+				padding: var(--nlt-spacing--sm) 0;
+			`}
+		>
+			<Button
+				icon={<Icon lucideId="plus" />}
+				ariaLabel="New row"
+				onClick={() => onClick()}
+			/>
 		</div>
 	);
 }
