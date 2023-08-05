41475fc7bed18a9d48cfe46e2679d2cb61df89bc
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-08-05 09:39:40 -0600

    refactor: replace with styles.css

--- src/react/loom-app/text-cell/index.tsx
+++ src/react/loom-app/text-cell/index.tsx
@@ -1,6 +1,5 @@
 import { useRenderMarkdown } from "src/shared/render-utils";
 import { useOverflow } from "src/shared/spacing/hooks";
-import { css } from "@emotion/react";
 import { appendOrReplaceFirstChild } from "src/shared/render/utils";
 
 interface Props {
@@ -10,32 +9,14 @@ interface Props {
 
 export default function TextCell({ markdown, shouldWrapOverflow }: Props) {
 	const { containerRef, renderRef } = useRenderMarkdown(markdown);
+	const overflowClassName = useOverflow(shouldWrapOverflow);
 
-	const overflowStyle = useOverflow(shouldWrapOverflow);
+	let className = "dataloom-text-cell";
+	className += " " + overflowClassName;
 	return (
-		<div
-			className="dataloom-text-cell"
-			css={css`
-				width: 100%;
-				height: 100%;
-				${overflowStyle}
-			`}
-		>
+		<div className={className}>
 			<div
-				css={css`
-					text-align: left;
-
-					p {
-						margin: 0;
-					}
-
-					//.contains-task-list is from dataview
-					//it is what is rendered for a task list
-					ul:not(.contains-task-list) {
-						padding-left: var(--nlt-spacing--xl);
-						padding-right: 0;
-					}
-				`}
+				className="dataloom-text-cell__container"
 				ref={(node) => {
 					containerRef.current = node;
 					appendOrReplaceFirstChild(node, renderRef.current);
