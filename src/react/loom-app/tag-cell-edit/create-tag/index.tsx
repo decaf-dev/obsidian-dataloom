8a07baf29352fc87b28df9a318a6133a13583003
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-08-05 09:39:50 -0600

    refactor: replace with styles.css

---
+++ src/react/loom-app/tag-cell-edit/create-tag/index.tsx
@@ -0,0 +1,25 @@
+import Tag from "src/react/shared/tag";
+
+import { Color } from "src/shared/loom-state/types";
+
+import Stack from "src/react/shared/stack";
+
+interface Props {
+	markdown: string;
+	color: Color;
+	onTagAdd: (markdown: string, color: Color) => void;
+}
+
+export default function CreateTag({ markdown, color, onTagAdd }: Props) {
+	return (
+		<button
+			className="dataloom-create-tag dataloom-focusable dataloom-selectable"
+			onClick={() => onTagAdd(markdown, color)}
+		>
+			<Stack spacing="sm" isHorizontal>
+				<div>Create</div>
+				<Tag markdown={markdown} color={color} maxWidth="120px" />
+			</Stack>
+		</button>
+	);
+}
