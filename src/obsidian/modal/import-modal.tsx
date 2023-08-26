5d1495917baf1aa84b5e890b5d957441e38f5402
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-08-26 13:07:16 -0600

    feat: add basic saving

--- src/obsidian/modal/import-modal.tsx
+++ src/obsidian/modal/import-modal.tsx
@@ -1,10 +1,15 @@
 import { App, Modal, Notice, TFile } from "obsidian";
+
 import { Root, createRoot } from "react-dom/client";
-import ImportApp from "../../react/import-app";
-import { deserializeLoomState } from "src/data/serialize";
 import { Provider } from "react-redux";
+
+import ImportApp from "../../react/import-app";
+
+import { deserializeLoomState, serializeLoomState } from "src/data/serialize";
 import { store } from "src/redux/store";
 import { renderDivider, setModalTitle } from "../shared";
+import { LoomState } from "src/shared/loom-state/types";
+import { EVENT_APP_REFRESH } from "src/shared/events";
 
 export default class ImportModal extends Modal {
 	root: Root;
@@ -13,7 +18,6 @@ export default class ImportModal extends Modal {
 
 	constructor(app: App, loomFile: TFile, pluginVersion: string) {
 		super(app);
-		this.app = app;
 		this.loomFile = loomFile;
 		this.pluginVersion = pluginVersion;
 	}
@@ -38,10 +42,7 @@ export default class ImportModal extends Modal {
 				<Provider store={store}>
 					<ImportApp
 						initialState={state}
-						onStateSave={() => {}}
-						// onStateSave={(state) =>
-						// 	this.handleStateSave(view, state)
-						// }
+						onStateSave={this.handleStateSave}
 					/>
 				</Provider>
 			);
@@ -50,13 +51,19 @@ export default class ImportModal extends Modal {
 		}
 	}
 
-	// private handleStateSave = (view: DataLoomView, state: LoomState) => {
-	// 	const serialized = serializeLoomState(state);
+	private handleStateSave = async (state: LoomState) => {
+		const serialized = serializeLoomState(state);
+		await this.app.vault.modify(this.loomFile, serialized);
 
-	// 	//Update the file contents and force an update of the React app
-	// 	view.setViewData(serialized, true);
-	// 	this.close();
-	// };
+		//Trigger an event to refresh the other open views of this file
+		this.app.workspace.trigger(
+			EVENT_APP_REFRESH,
+			this.loomFile.path,
+			"",
+			state
+		);
+		this.close();
+	};
 
 	onClose() {
 		if (this.root) this.root.unmount();
