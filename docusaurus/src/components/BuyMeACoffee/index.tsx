fcb61d6eb11b555478e2b3cef924bcf0611ae6d8
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-09-09 14:56:34 -0600

    refactor: remove redundant code

--- docusaurus/src/components/BuyMeACoffee/index.tsx
+++ docusaurus/src/components/BuyMeACoffee/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function BuyMeACoffee(): JSX.Element {
-	const [refreshTime] = React.useState(Date.now());
 	return (
 		<a
 			href="https://buymeacoffee.com/treywallis"
@@ -10,7 +9,7 @@ export default function BuyMeACoffee(): JSX.Element {
 		>
 			<img
 				width="180"
-				src={`https://img.buymeacoffee.com/button-api/?text=Buy me a herbal tea&emoji=🍵&slug=treywallis&button_colour=9478F0&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00&time=${refreshTime}"`}
+				src={`https://img.buymeacoffee.com/button-api/?text=Buy me a herbal tea&emoji=🍵&slug=treywallis&button_colour=9478F0&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00&time=${Date.now()}"`}
 				alt="Buymeacoffee"
 			/>
 		</a>
