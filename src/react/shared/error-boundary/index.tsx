ca5d7dcfe49e59238cf86fe7e411163a471db49e
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-09-27 23:31:09 -0600

    feat: add error boundary

---
+++ src/react/shared/error-boundary/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./styles.css";
+import ErrorDisplay from "../error-display";
+
+interface ErrorBoundaryProps {
+	children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+	hasError: boolean;
+	errorMessage?: string;
+	errorInfo?: React.ErrorInfo;
+}
+
+class ErrorBoundary extends React.Component<
+	ErrorBoundaryProps,
+	ErrorBoundaryState
+> {
+	constructor(props: ErrorBoundaryProps) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+		this.setState({
+			hasError: true,
+			errorInfo,
+			errorMessage: error.message,
+		});
+	}
+
+	render() {
+		let copyErrorMessage = "";
+		if (this.state.hasError) {
+			copyErrorMessage = `Error message: ${this.state.errorMessage}\nError info: ${this.state.errorInfo?.componentStack}`;
+			return (
+				<ErrorDisplay
+					title="DataLoom experienced an error"
+					errorMessage={this.state.errorMessage ?? ""}
+					copyErrorMessage={copyErrorMessage}
+				/>
+			);
+		}
+
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary;
---
+++ src/react/shared/error-display/index.tsx
@@ -0,0 +1,71 @@
+import { Notice } from "obsidian";
+import Divider from "../divider";
+import Padding from "../padding";
+import Stack from "../stack";
+import Text from "../text";
+
+import "./styles.css";
+
+interface Props {
+	title: string;
+	errorMessage: string;
+	copyErrorMessage: string;
+	infoSection?: React.ReactNode;
+	isEmbeddedApp?: boolean;
+}
+
+export default function ErrorDisplay({
+	title,
+	copyErrorMessage,
+	errorMessage,
+	isEmbeddedApp,
+	infoSection,
+}: Props) {
+	function handleCopyClick() {
+		navigator.clipboard.writeText(copyErrorMessage);
+		new Notice("Copied error to clipboard");
+	}
+
+	let className = "dataloom-error";
+	if (isEmbeddedApp) className += " dataloom-error--embedded-app";
+
+	return (
+		<div className={className}>
+			<Padding p="2xl">
+				<Stack spacing="xl">
+					<Stack>
+						<Text variant="semibold" size="xl" value="Opps" />
+						<Text variant="semibold" size="md" value={title} />
+					</Stack>
+					<Divider />
+					<Stack>
+						<Text
+							variant="semibold"
+							size="md"
+							value="Error message"
+						/>
+						<div className="dataloom-error__message">
+							<Text value={errorMessage} whiteSpace="pre-wrap" />
+						</div>
+					</Stack>
+					<Divider />
+					{infoSection}
+					<Stack spacing="sm">
+						<Text value="For help fixing this error please visit:" />
+						<a href="https://dataloom.xyz/other/loom-file">
+							https://dataloom.xyz/other/loom-file
+						</a>
+					</Stack>
+					<Stack isHorizontal>
+						<button
+							className="dataloom-copy-button"
+							onClick={() => handleCopyClick()}
+						>
+							Copy to clipboard
+						</button>
+					</Stack>
+				</Stack>
+			</Padding>
+		</div>
+	);
+}
