4a0426a30358d2418601e9d1cef1db9361a9fc0b
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-10-02 17:55:13 -0600

    fix: resolve add comand args

--- src/react/loom-app/app/hooks/use-source/index.ts
+++ src/react/loom-app/app/hooks/use-source/index.ts
@@ -3,19 +3,14 @@ import { useLogger } from "src/shared/logger";
 import { useLoomState } from "../../../loom-state-provider";
 import SourceAddCommand from "src/shared/loom-state/commands/source-add-command";
 import SourceDeleteCommand from "src/shared/loom-state/commands/source-delete-command";
-import { useAppMount } from "src/react/loom-app/app-mount-provider";
-import FileCache from "src/shared/file-cache";
 
 export const useSource = () => {
 	const logger = useLogger();
-	const { app } = useAppMount();
 	const { doCommand } = useLoomState();
 
 	function handleSourceAdd(type: SourceType, name: string) {
 		logger("handleSourceAdd");
-		doCommand(
-			new SourceAddCommand(app, FileCache.getInstance(), type, name)
-		);
+		doCommand(new SourceAddCommand(type, name));
 	}
 
 	function handleSourceDelete(id: string) {
