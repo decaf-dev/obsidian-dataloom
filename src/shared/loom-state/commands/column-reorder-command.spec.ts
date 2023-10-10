861bbf3c77ccbcce4a3e87adf436de79552fe370
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-10-09 21:23:54 -0600

    Fix column reorder (#807)
    * fix: fix #799
    
    * docs: remove unneeded file
    
    * feat: add undo/redo for column order

---
+++ src/shared/loom-state/commands/column-reorder-command.spec.ts
@@ -0,0 +1,86 @@
+import { createTestLoomState } from "src/shared/loom-state/loom-state-factory";
+import CommandUndoError from "./command-undo-error";
+import CommandRedoError from "./command-redo-error";
+import ColumnReorderCommand from "./column-reorder-command";
+
+describe("column-update-command", () => {
+	it("should throw an error when undo() is called before execute()", () => {
+		//Arrange
+		const prevState = createTestLoomState(3, 1);
+		const firstColumn = prevState.model.columns[0].id;
+		const lastColumn = prevState.model.columns[2].id;
+		const command = new ColumnReorderCommand(lastColumn, firstColumn);
+
+		try {
+			//Act
+			command.undo(prevState);
+		} catch (err) {
+			expect(err).toBeInstanceOf(CommandUndoError);
+		}
+	});
+
+	it("should throw an error when redo() is called before undo()", () => {
+		//Arrange
+		const prevState = createTestLoomState(3, 1);
+		const firstColumn = prevState.model.columns[0].id;
+		const lastColumn = prevState.model.columns[2].id;
+		const command = new ColumnReorderCommand(lastColumn, firstColumn);
+
+		try {
+			//Act
+			command.execute(prevState);
+			command.redo(prevState);
+		} catch (err) {
+			expect(err).toBeInstanceOf(CommandRedoError);
+		}
+	});
+
+	it("moves the last column to the first column index when execute() is called", async () => {
+		//Arrange
+		const prevState = createTestLoomState(3, 1);
+		const firstColumn = prevState.model.columns[0].id;
+		const lastColumn = prevState.model.columns[2].id;
+		const command = new ColumnReorderCommand(lastColumn, firstColumn);
+
+		//Act
+		const executeState = command.execute(prevState);
+
+		//Assert
+		expect(executeState.model.columns[0].id).toEqual(lastColumn);
+		expect(executeState.model.columns[1].id).toEqual(firstColumn);
+		expect(executeState.model.columns[2].id).toEqual(
+			prevState.model.columns[1].id
+		);
+	});
+
+	it("updates the column order to the original order when undo() is called", () => {
+		//Arrange
+		const prevState = createTestLoomState(3, 1);
+		const firstColumn = prevState.model.columns[0].id;
+		const lastColumn = prevState.model.columns[2].id;
+		const command = new ColumnReorderCommand(lastColumn, firstColumn);
+
+		//Act
+		const executeState = command.execute(prevState);
+		const undoState = command.undo(executeState);
+
+		//Assert
+		expect(undoState.model.columns).toEqual(prevState.model.columns);
+	});
+
+	it("updates the column order to the modified order redo() is called", () => {
+		//Arrange
+		const prevState = createTestLoomState(3, 1);
+		const firstColumn = prevState.model.columns[0].id;
+		const lastColumn = prevState.model.columns[2].id;
+		const command = new ColumnReorderCommand(lastColumn, firstColumn);
+
+		//Act
+		const executeState = command.execute(prevState);
+		const undoState = command.undo(executeState);
+		const redoState = command.redo(undoState);
+
+		//Assert
+		expect(redoState.model.columns).toEqual(executeState.model.columns);
+	});
+});
---
+++ src/shared/loom-state/commands/column-reorder-command.ts
@@ -0,0 +1,128 @@
+import { LoomState } from "../types";
+import LoomStateCommand from "./loom-state-command";
+import { Column, Row } from "../types/loom-state";
+
+export default class ColumnReorderCommand extends LoomStateCommand {
+	private dragId: string;
+	private targetId: string;
+
+	private dragIndex: number;
+	private targetIndex: number;
+
+	constructor(dragId: string, targetId: string) {
+		super();
+		this.dragId = dragId;
+		this.targetId = targetId;
+	}
+
+	execute(prevState: LoomState): LoomState {
+		super.onExecute();
+
+		const { columns } = prevState.model;
+
+		const dragIndex = columns.findIndex(
+			(column) => column.id === this.dragId
+		);
+		const targetIndex = columns.findIndex(
+			(column) => column.id === this.targetId
+		);
+
+		this.dragIndex = dragIndex;
+		this.targetIndex = targetIndex;
+
+		const newColumns = [...columns];
+		const draggedEl = newColumns[dragIndex];
+
+		//Remove the element
+		newColumns.splice(dragIndex, 1);
+		//Append it to the new location
+		newColumns.splice(targetIndex, 0, draggedEl);
+
+		const nextRows = this.sortCellsByColumns(
+			prevState.model.rows,
+			newColumns
+		);
+
+		return {
+			...prevState,
+			model: {
+				...prevState.model,
+				columns: newColumns,
+				rows: nextRows,
+			},
+		};
+	}
+
+	undo(prevState: LoomState): LoomState {
+		super.onUndo();
+
+		const { columns } = prevState.model;
+
+		const newColumns = [...columns];
+		const draggedEl = newColumns[this.targetIndex];
+
+		//Remove the element
+		newColumns.splice(this.targetIndex, 1);
+		//Append it to the new location
+		newColumns.splice(this.dragIndex, 0, draggedEl);
+
+		const nextRows = this.sortCellsByColumns(
+			prevState.model.rows,
+			newColumns
+		);
+
+		return {
+			...prevState,
+			model: {
+				...prevState.model,
+				columns: newColumns,
+				rows: nextRows,
+			},
+		};
+	}
+	redo(prevState: LoomState): LoomState {
+		super.onRedo();
+
+		const { columns } = prevState.model;
+		const newColumns = [...columns];
+		const draggedEl = newColumns[this.dragIndex];
+
+		//Remove the element
+		newColumns.splice(this.dragIndex, 1);
+		//Append it to the new location
+		newColumns.splice(this.targetIndex, 0, draggedEl);
+
+		const nextRows = this.sortCellsByColumns(
+			prevState.model.rows,
+			newColumns
+		);
+
+		return {
+			...prevState,
+			model: {
+				...prevState.model,
+				columns: newColumns,
+				rows: nextRows,
+			},
+		};
+	}
+
+	private sortCellsByColumns(prevRows: Row[], columns: Column[]): Row[] {
+		return prevRows.map((row) => {
+			const { cells } = row;
+			const nextCells = cells.sort((a, b) => {
+				const aIndex = columns.findIndex(
+					(column) => column.id === a.columnId
+				);
+				const bIndex = columns.findIndex(
+					(column) => column.id === b.columnId
+				);
+				return aIndex - bIndex;
+			});
+			return {
+				...row,
+				cells: nextCells,
+			};
+		});
+	}
+}
